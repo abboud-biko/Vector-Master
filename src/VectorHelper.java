9bdc932ee7efeabca103fe8f6de158fe65de615a
Yassino01 <fy_bareche@esi.dz>
2017-10-19 13:30:34 +0100

    Creating VectorHelper Class

---
+++ src/VectorHelper.java
@@ -0,0 +1,12 @@
+public class VectorHelper {
+
+    private int[] vector;
+    private int size;
+
+    public VectorHelper(int[] vector) {
+        this.size = vector.length;
+        this.vector = vector;
+    }
+
+    
+}
