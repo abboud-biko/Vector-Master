50ac2cd1951d8f8be730c993d79597bcaf7ff021
abboud-biko <fe_abboud@esi.dz>
2017-10-19 12:50:17 +0100

    add methode sortVector

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -7,6 +7,23 @@ public class VectorHelper {
         this.size = vector.length;
         this.vector = vector;
     }
-
+    public int[] sortVector(){
+        int i = 0;
+        while ( i < vector.length ){
+            int index = i;
+            int min = vector[i];
+            for (int j=i; j<vector.length; j++){
+                if ( vector[j] < min){
+                    min = vector[j];
+                    index = j;
+                }
+            }
+            int s = vector[i];
+            vector[i] = vector[index];
+            vector[index] = s;
+            i++;
+        }
+        return vector;
+    }
     
 }
